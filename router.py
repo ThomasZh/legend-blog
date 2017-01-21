18edad91f73ed3ca7ef13916b1815b3df29689ca
ThomasZhang <thomas.zh@qq.com>
2017-01-22 06:25:50 +0800

    添加电话号码注册逻辑

--- router.py
+++ router.py
@@ -9,13 +9,13 @@ import tornado.web
 
 from foo import comm
 from foo.auth import auth_email
+from foo.auth import auth_phone
 
 
 def map():
 
     config = [
 
-        # GET: 根据 HTTP header 收集客户端相关信息：是否手机、操作系统、浏览器等信息。
         (r'/', getattr(auth_email, 'AuthWelcomeHandler')),
         (r'/auth/email/login', getattr(auth_email, 'AuthEmailLoginHandler')),
         (r'/auth/email/register', getattr(auth_email, 'AuthEmailRegisterHandler')),
@@ -23,6 +23,10 @@ def map():
         (r'/auth/email/reset-pwd', getattr(auth_email, 'AuthEmailResetPwdHandler')),
         (r'/auth/welcome', getattr(auth_email, 'AuthWelcomeHandler')),
         (r'/auth/logout', getattr(auth_email, 'AuthLogoutHandler')),
+        (r'/auth/phone/login', getattr(auth_phone, 'AuthPhoneLoginHandler')),
+        (r'/auth/phone/register', getattr(auth_phone, 'AuthPhoneRegisterHandler')),
+        (r'/auth/phone/verify-code', getattr(auth_phone, 'AuthPhoneVerifyCodeHandler')),
+        (r'/auth/phone/lost-pwd', getattr(auth_phone, 'AuthPhoneLostPwdHandler')),
 
         # comm
         ('.*', getattr(comm, 'PageNotFoundHandler'))
